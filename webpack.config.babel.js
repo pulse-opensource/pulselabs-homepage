e6ebe1b10a51916ee91fd4a2043603bbe6dcb40c
Sean Larkin <sean.larkin@cuw.edu>
2016-11-15 12:41:07 -0600

    chore(setup): initial commits to homepage

---
+++ webpack.config.babel.js
@@ -0,0 +1,37 @@
+import {join, resolve} from 'path';
+import webpack, {ProgressPlugin} from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+
+const config = {
+	entry: {
+		main: './src/index'
+	},
+	output: {
+		path: resolve(__dirname, './dist'),
+		filename: '[name].chunk.js'
+	},
+	resolve: {
+		alias: {
+			"react": "preact-compat",
+			"react-dom": "preact-compat"
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				loader: 'babel-loader',
+				exclude: [/node_module/]
+			}
+		]
+	},
+	plugins: [
+		new ProgressPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		})
+	]
+};
+
+export default config;
