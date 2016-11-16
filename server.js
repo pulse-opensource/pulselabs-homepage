a08f209b26e526dbd7e38e3070acdeb86bc4d473
Cole Voss <voss.cole@gmail.com>
2016-11-16 13:34:30 -0600

    #10 remove old code

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const options = {
 };
 
 
-// const appJs = fs.readFileSync(`${__dirname}/../dist/main.chunk.js`);
 const indexHTML = fs.readFileSync(path.join(__dirname, '/dist/index.html'));
 
 const FILES = [
@@ -32,25 +31,6 @@ const onRequest = (request, response) => {
   
   response.end(indexHTML);
 };
-
-
-// const server = spdy.createServer(options, (request, response) => {
-//   const headers = {
-//     'content-type': 'application/javascript',
-//   };
-
-//   response.push('/main.chunk.js', headers, (err, stream) => {
-//     if (err) return;
-
-//     stream.end(appJs);
-//   });
-
-//   response.writeHead(200, {
-//     'content-type': 'text/html',
-//   });
-
-//   response.end(indexHTML);
-// });
 
 
 const server = http2.createServer(options, onRequest);
